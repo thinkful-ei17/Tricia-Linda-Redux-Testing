1def71aa0b6d0a3892427286696673fd1755ba33
TriciaF <TriciaF@thinkful.com>
2018-02-01 17:30:23 -0500

    completed Redux tests

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-
+import {makeGuess} from '../actions';
 import {GuessForm} from './guess-form';
 
 describe('<GuessForm />', () => {
@@ -10,21 +10,20 @@ describe('<GuessForm />', () => {
 
   it('Should fire the onMakeGuess callback when the form is submitted', () => {
     const callback = jest.fn();
-    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
-    const value = 10;
+    const wrapper = mount(<GuessForm dispatch={callback} />);
+    const value = "10";
     wrapper.find('input[type="number"]').instance().value = value;
     wrapper.simulate('submit');
-    expect(callback).toHaveBeenCalledWith(value.toString());
+    expect(callback).toHaveBeenCalledWith(makeGuess(value));
   });
 
   it('Should reset the input when the form is submitted', () => {
-    const spy = jest.fn();
-    const wrapper = mount(<GuessForm dispatch={spy}/>);
+    const wrapper = mount(<GuessForm dispatch={()=>{}}/>);
+    // console.log(wrapper.debug());
     const input = wrapper.find('input[type="number"]');
-    console.log('this is the input: ', input.debug());
     input.instance().value = 10;
     wrapper.simulate('submit');
-    input.instance().value = "";
-    expect(spy).toHaveBeenCalledWith(input.instance().value);
+    const value = "";
+    expect(input.instance().value).toEqual(value);
   });
 });
--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import {restartGame} from '../actions';
 import {TopNav} from './top-nav';
 
 describe('<TopNav />', () => {
@@ -10,17 +10,22 @@ describe('<TopNav />', () => {
 
   it('Should call onNewGame when new game is clicked', () => {
     const callback = jest.fn();
-    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const wrapper = shallow(<TopNav dispatch={callback} />);
     const link = wrapper.find('.new');
     link.simulate('click', {
       preventDefault() {}
     });
     expect(callback).toHaveBeenCalled();
+    const action = callback.mock.calls[0][0];
+    expect(action.type).toEqual('RESTART_GAME');
+    console.log(callback.mock.calls);
+    expect(action.correctAnswer).toBeGreaterThan(0);
+    expect(action.correctAnswer).toBeLessThan(101);
   });
 
   it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
     const callback = jest.fn();
-    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+    const wrapper = shallow(<TopNav dispatch={callback} />);
     const link = wrapper.find('.status-link');
     link.simulate('click', {
       preventDefault() {}
